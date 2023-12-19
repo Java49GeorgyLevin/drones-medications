954b95165bb42f4edf8f8684395f19012d558cc8
Georgy Levin <g.phyzy@gmail.com>
2023-12-19 19:52:37 +0200

    initial commit

---
+++ src/main/java/telran/drones/DronesMedicationsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package telran.drones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesMedicationsWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesMedicationsWebServiceApplication.class, args);
+	}
+
+}

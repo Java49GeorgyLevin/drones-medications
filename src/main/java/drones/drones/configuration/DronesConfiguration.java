714db3462ca7edc7ce05338f6f8542f319cd34d0
Georgy Levin <g.phyzy@gmail.com>
2023-12-23 08:36:56 +0200

    Cofiguration, EventLog & LogDto

---
+++ src/main/java/drones/drones/configuration/DronesConfiguration.java
@@ -0,0 +1,14 @@
+package drones.drones.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DronesConfiguration {
+	@Bean
+	ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+
+}
